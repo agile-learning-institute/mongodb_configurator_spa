5a958f61c3f689252400f38dd34ca92ea2d1b83a
Mike Storey <mike@agile-learning.institute>
2025-07-22 22:56:30 -0400

    Cleanup: Remove unused addItem, removeItem, and updateItem functions from useTypeArrayOfObjectPropertyEditor.ts

--- src/composables/useTypeArrayOfObjectPropertyEditor.ts
+++ src/composables/useTypeArrayOfObjectPropertyEditor.ts
@@ -24,20 +24,6 @@ export function useTypeArrayOfObjectPropertyEditor(
     }
   })
 
-  // Add a new item to the array
-  function addItem() {
-    if (!property.value.items) {
-      property.value.items = {
-        description: 'New object item',
-        type: 'object',
-        required: false,
-        properties: {}
-      }
-    }
-    emit('change', { ...property.value })
-  }
-
-  // Remove a property by key
   function handleDeleteProperty(key: string) {
     if (property.value.items?.properties && property.value.items.properties[key]) {
       delete property.value.items.properties[key]
@@ -51,18 +37,6 @@ export function useTypeArrayOfObjectPropertyEditor(
       property.value.items.properties[key] = updated
       emit('change', { ...property.value })
     }
-  }
-
-  // Remove an item from the array
-  function removeItem(index: number) {
-    // TODO: Implement array item removal logic
-    console.log('Remove item at index:', index)
-  }
-
-  // Update an item in the array
-  function updateItem(index: number, updated: TypeProperty) {
-    // TODO: Implement array item update logic
-    console.log('Update item at index:', index, updated)
   }
 
   function handleAddProperty() {
