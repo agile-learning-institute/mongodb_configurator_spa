da943603c0b6d2d2caf6c7235197b6891bf9a6d1
Mike Storey <mike@agile-learning.institute>
2025-08-05 12:20:50 -0400

    Reduce enumerator name input field width
    - Change min-width from 200px to 100px (50% reduction)
    - Makes input field more compact and space-efficient
    - Maintains functionality while improving layout density

--- src/pages/EnumeratorDetailPage.vue
+++ src/pages/EnumeratorDetailPage.vue
@@ -107,7 +107,7 @@
                 hide-details
                 :disabled="enumerator._locked"
                 class="mr-3"
-                style="min-width: 200px;"
+                style="min-width: 100px;"
                 @blur="finishEnumNameEdit(enumIdx)"
                 @keyup.enter="finishEnumNameEdit(enumIdx)"
               />
