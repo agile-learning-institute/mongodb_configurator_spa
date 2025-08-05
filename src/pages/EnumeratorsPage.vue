3237499c2bff20f6f3a4d62463b64c47b61f72ef
Mike Storey <mike@agile-learning.institute>
2025-08-05 13:50:44 -0400

    Add version-aware Add Enumerators button
    - Add Add Enumerators button to EnumeratorsPage
    - Check existing files to determine next version number
    - Prevents overwriting existing files by calculating highest version + 1
    - Creates new enumerator file with proper version numbering
    - Navigates to new file for immediate editing

--- src/pages/EnumeratorsPage.vue
+++ src/pages/EnumeratorsPage.vue
@@ -4,6 +4,15 @@
       <h3>Enumerators</h3>
       <div class="d-flex align-center">
         <v-btn
+          color="primary"
+          variant="elevated"
+          prepend-icon="mdi-plus"
+          @click="handleAddEnumerators"
+          class="mr-2"
+        >
+          Add Enumerators
+        </v-btn>
+        <v-btn
           v-if="canLockAll"
           color="info"
           variant="outlined"
@@ -28,6 +37,7 @@
 import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import FileList from '@/components/FileList.vue'
+import { apiService } from '@/utils/api'
 
 const router = useRouter()
 const fileListRef = ref()
@@ -42,6 +52,46 @@ const handleEdit = (fileName: string) => {
 
 const handleOpen = (fileName: string) => {
   router.push(`/enumerators/${fileName}`)
+}
+
+const handleAddEnumerators = async () => {
+  try {
+    // Get the list of existing enumerator files
+    const existingFiles = await apiService.getEnumerators()
+    
+    // Find the highest version number
+    let maxVersion = 0
+    existingFiles.forEach((file: any) => {
+      const match = file.file_name.match(/enumerations\.(\d+)\.yaml/)
+      if (match) {
+        const version = parseInt(match[1], 10)
+        if (version > maxVersion) {
+          maxVersion = version
+        }
+      }
+    })
+    
+    // Create new version number
+    const newVersion = maxVersion + 1
+    const newFileName = `enumerations.${newVersion}.yaml`
+    
+    // Create the initial enumerator data
+    const newEnumeratorData = {
+      file_name: newFileName,
+      version: newVersion,
+      enumerators: [],
+      _locked: false
+    }
+    
+    // Save the new enumerator file
+    await apiService.saveEnumerator(newFileName, newEnumeratorData)
+    
+    // Navigate to the new enumerator
+    router.push(`/enumerators/${newFileName}`)
+  } catch (err: any) {
+    console.error('Failed to create new enumerator:', err)
+    // You might want to show an error message to the user here
+  }
 }
 
 const handleLockAll = async () => {
