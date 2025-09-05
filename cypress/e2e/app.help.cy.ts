b98d0c2ba89c50db5525329d2d6c0f03540fd3d5
Mike Storey <mike@agile-learning.institute>
2025-09-04 22:34:44 -0400

    needed to wait for page load

--- cypress/e2e/app.help.cy.ts
+++ cypress/e2e/app.help.cy.ts
@@ -43,12 +43,14 @@ describe('App Help Pages', () => {
 
   it('can navigate to the help page from configuration pages', () => {
     cy.visit('/configurations')
+    cy.wait(100)
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Collection Configuration')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="page-title"]').should('contain.text', 'Collection Configuration')
 
     cy.visit('/configurations/sample.yaml')
+    cy.wait(100)
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Collection Configuration')
     cy.get('[data-test="help-btn"]').click()
@@ -57,6 +59,7 @@ describe('App Help Pages', () => {
 
   it('can navigate to the help page from dictionary pages', () => {
     cy.visit('/dictionaries')
+    cy.wait(100)
     cy.get('[data-test="page-title').should('contain.text', 'Dictionaries')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Dictionary')
@@ -64,6 +67,7 @@ describe('App Help Pages', () => {
     cy.get('[data-test="page-title').should('contain.text', 'Dictionaries')
 
     cy.visit('/dictionaries/sample.1.0.1.yaml')
+    cy.wait(100)
     cy.get('header').should('contain.text', 'sample.1.0.1.yaml')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Dictionary')
@@ -73,6 +77,7 @@ describe('App Help Pages', () => {
 
   it('can navigate to the help page from type pages', () => {
     cy.visit('/types')
+    cy.wait(100)
     cy.get('[data-test="page-title').should('contain.text', 'Types')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Type')
@@ -80,6 +85,7 @@ describe('App Help Pages', () => {
     cy.get('[data-test="page-title').should('contain.text', 'Types')
 
     cy.visit('/types/word.yaml')
+    cy.wait(100)
     cy.get('header').should('contain.text', 'Type: word')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Type')
@@ -89,6 +95,7 @@ describe('App Help Pages', () => {
 
   it('can navigate to the help page from enumerators pages', () => {
     cy.visit('/enumerators')
+    cy.wait(100)
     cy.get('h3').should('contain.text', 'Enumerators')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Enumerator')
@@ -96,6 +103,7 @@ describe('App Help Pages', () => {
     cy.get('h3').should('contain.text', 'Enumerators')
 
     cy.visit('/enumerators/enumerations.0.yaml')
+    cy.wait(100)
     cy.get('[data-test="enumerator-version"]').should('contain.text', 'Version: 0')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Enumerator')
@@ -105,6 +113,7 @@ describe('App Help Pages', () => {
 
   it('can navigate to the help page from Test Data pages', () => {
     cy.visit('/test_data')
+    cy.wait(100)
     cy.get('h3').should('contain.text', 'Test Data')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Test Data')
@@ -112,6 +121,7 @@ describe('App Help Pages', () => {
     cy.get('h3').should('contain.text', 'Test Data')
 
     cy.visit('/test_data/sample.1.0.0.1.json')
+    cy.wait(100)
     cy.get('[data-test="card-title"]').should('contain.text', 'sample.1.0.0.1.json')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Test Data')
@@ -121,6 +131,7 @@ describe('App Help Pages', () => {
 
   it('can navigate to the help page from Migration pages', () => {
     cy.visit('/migrations')
+    cy.wait(100)
     cy.get('h3').should('contain.text', 'Migrations')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Migration')
@@ -128,6 +139,7 @@ describe('App Help Pages', () => {
     cy.get('h3').should('contain.text', 'Migrations')
 
     cy.visit('/migrations/first_last_to_full_name.json')
+    cy.wait(100)
     cy.get('[data-test="card-title"]').should('contain.text', 'first_last_to_full_name.json')
     cy.get('[data-test="help-btn"]').click()
     cy.get('[data-test="carousel-title-text').should('contain.text', 'Migration')
@@ -137,6 +149,7 @@ describe('App Help Pages', () => {
 
   it('can navigate to the help page from Event Viewer page', () => {
     cy.visit('/')
+    cy.wait(100)
     cy.get('[data-test="process-all-btn"]').click()
 
     cy.get('h1').first().should('contain.text', 'Processing Complete')
